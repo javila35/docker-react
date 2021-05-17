f9b2a1c16fe3c9ae0e62b180fe0caca10fda6f60
javila35 <josepharthuravila@gmail.com>
2021-05-16 20:09:00 -0700

    Iniital commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders joe's hello world message", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = screen.getByText(/hi king/i);
+  expect(text).toBeInTheDocument();
 });
